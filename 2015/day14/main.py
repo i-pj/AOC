89ffa32be7c07ec3c3965f799a07168be1736a96
Parth Jain <42parth.jain@gmail.com>
2024-05-13 12:26:13 +0530

    day14

--- 2015/day14/main.py
+++ 2015/day14/main.py
@@ -6,7 +6,7 @@ with open('input.txt', 'r') as f:
 reindeer = {}
 for line in lines:
     name, speed, fly_time, rest_time = re.match(r'(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.', line).groups()
-    reindeer[name] = {'speed': int(speed), 'fly_time': int(fly_time),'rest_time': int(rest_time), 'distance': 0,'state': 'flying', 'time_left': int(fly_time)}
+    reindeer[name] = {'speed': int(speed), 'fly_time': int(fly_time),'rest_time': int(rest_time), 'distance': 0,'state': 'flying', 'time_left': int(fly_time), 'points': 0}
 
 for _ in range(2503):
     for name, info in reindeer.items():
@@ -21,5 +21,9 @@ for _ in range(2503):
             if info['time_left'] == 0:
                 info['state'] = 'flying'
                 info['time_left'] = info['fly_time']
+    leaders = [name for name, info in reindeer.items() if info['distance'] == max(info['distance'] for info in reindeer.values())]
+    for leader in leaders:
+        reindeer[leader]['points'] += 1
 
-print(max(info['distance'] for info in reindeer.values()))
+print("Part 1:", max(info['distance'] for info in reindeer.values()))
+print("Part 2:", max(info['points'] for info in reindeer.values()))
