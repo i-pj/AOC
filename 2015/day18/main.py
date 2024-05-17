3a6d2880afc7ed8acc676ec440ed2adc49e19d01
Parth Jain <42parth.jain@gmail.com>
2024-05-17 18:31:21 +0530

    day18

--- 2015/day18/main.py
+++ 2015/day18/main.py
@@ -8,23 +8,30 @@ def count_neighbors(grid, i, j):
                 count += 1
     return count
 
-def step(grid):
+def step(grid, stuck_corners=False):
     new_grid = np.copy(grid)
     for i in range(100):
         for j in range(100):
-            neighbors = count_neighbors(grid, i, j)
-            if grid[i, j] == 1 and (neighbors < 2 or neighbors > 3):
-                new_grid[i, j] = 0
-            elif grid[i, j] == 0 and neighbors == 3:
+            if stuck_corners and ((i, j) in [(0, 0), (0, 99), (99, 0), (99, 99)]):
                 new_grid[i, j] = 1
+            else:
+                neighbors = count_neighbors(grid, i, j)
+                if grid[i, j] == 1 and (neighbors < 2 or neighbors > 3):
+                    new_grid[i, j] = 0
+                elif grid[i, j] == 0 and neighbors == 3:
+                    new_grid[i, j] = 1
     return new_grid
 
-def solve(initial_grid):
+def solve(initial_grid, stuck_corners=False):
     grid = np.array([[1 if c == '#' else 0 for c in row] for row in initial_grid.splitlines()])
+    if stuck_corners:
+        grid[0, 0] = grid[0, 99] = grid[99, 0] = grid[99, 99] = 1
     for _ in range(100):
-        grid = step(grid)
+        grid = step(grid, stuck_corners)
     return np.sum(grid)
 
 with open('input.txt') as f:
     initial_grid = f.read()
-print(solve(initial_grid))
+
+print("Part 1:", solve(initial_grid))
+print("Part 2:", solve(initial_grid, stuck_corners=True))
