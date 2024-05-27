3b0c7552a1b27d22a571c5e2c2d0935133be3576
Parth Jain <42parth.jain@gmail.com>
2024-05-27 23:40:23 +0530

    day2

--- 2023/day2/main.py
+++ 2023/day2/main.py
@@ -1,25 +1,45 @@
+# max_cubes = {"red": 12, "green": 13, "blue": 14}
+# result = [] # list of games that are possible
+# 
+# with open("input.txt") as f:
+#     for line in f:
+#         game_id, sets = line.split(": ")
+#         #print(game_id)
+#         #print(sets)
+#         game_id = int(game_id.split()[1])
+#         #print(game_id)
+#         possible = True
+#         for s in sets.split("; "):
+#             #print(s)
+#             for cube in s.split(", "):
+#                 #print(cube)
+#                 count, color = cube.split()
+#                 if int(count) > max_cubes[color]:
+#                     possible = False
+#                     break
+#             if not possible:
+#                 break
+#         if possible:
+#             result.append(game_id)
+# 
+# print(sum(result))
 max_cubes = {"red": 12, "green": 13, "blue": 14}
-result = [] # list of games that are possible
+result = []  # list of games with minimum cube counts
 
 with open("input.txt") as f:
     for line in f:
         game_id, sets = line.split(": ")
-        #print(game_id)
-        #print(sets)
         game_id = int(game_id.split()[1])
-        #print(game_id)
-        possible = True
+        min_cubes = {"red": 0, "green": 0, "blue": 0}
         for s in sets.split("; "):
-            #print(s)
             for cube in s.split(", "):
-                #print(cube)
                 count, color = cube.split()
-                if int(count) > max_cubes[color]:
-                    possible = False
-                    break
-            if not possible:
-                break
-        if possible:
-            result.append(game_id)
+                min_cubes[color] = max(min_cubes[color], int(count))
+        result.append(min_cubes)
 
-print(sum(result))
+power_sum = 0
+for game in result:
+    power = game["red"] * game["green"] * game["blue"]
+    power_sum += power
+
+print(power_sum)
