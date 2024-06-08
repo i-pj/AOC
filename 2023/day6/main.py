235035f5c4591f3d238ad33de474a4fc453531eb
Parth Jain <42parth.jain@gmail.com>
2024-06-08 23:22:57 +0530

    day6

--- 2023/day6/main.py
+++ 2023/day6/main.py
@@ -1,20 +1,34 @@
+# with open('input.txt') as f:
+#     data = f.read().strip()
+# 
+# lines = data.split("\n")
+# times = list(map(int, [x for x in lines[0].split() if x.isdigit()]))
+# distances = list(map(int, [x for x in lines[1].split() if x.isdigit()]))
+# 
+# ways_to_win = 1
+# 
+# for t, d in zip(times, distances):
+#     count = 0
+#     for speed in range(1, t):
+#         total = (t - speed) * speed
+# 
+#         if total > d:
+#             count += 1
+# 
+#     ways_to_win *= count
+# 
+# print(ways_to_win)
+
 with open('input.txt') as f:
-    data = f.read().strip()
+    lines = f.readlines()
 
-lines = data.split("\n")
-times = list(map(int, [x for x in lines[0].split() if x.isdigit()]))
-distances = list(map(int, [x for x in lines[1].split() if x.isdigit()]))
+time = int(''.join(filter(str.isdigit, lines[0])))
+distance = int(''.join(filter(str.isdigit, lines[1])))
 
-ways_to_win = 1
+count = 0
+for speed in range(1, time):
+    total = (time - speed) * speed
+    if total > distance:
+        count += 1
 
-for t, d in zip(times, distances):
-    count = 0
-    for speed in range(1, t):
-        total = (t - speed) * speed
-
-        if total > d:
-            count += 1
-
-    ways_to_win *= count
-
-print(ways_to_win)
+print(count)
