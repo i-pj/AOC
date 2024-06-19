5873f001157abf62a21d27971c58f36ceb6e8a20
Parth Jain <42parth.jain@gmail.com>
2024-06-19 18:12:18 +0530

    day4

--- 2023/day4/main.py
+++ 2023/day4/main.py
@@ -1,4 +1,14 @@
 import re
+# 
+# with open("input.txt") as f:
+#     lines = f.read().strip().split("\n")
+# 
+# cards = [list(map(int, re.findall(r"\d+", line))) for line in lines]
+# wins = [len(set(card[11:]) & set(card[1:11])) for card in cards]
+# 
+# print(sum(2 ** (match_count - 1) for match_count in wins if match_count > 0))
+
+from collections import defaultdict
 
 with open("input.txt") as f:
     lines = f.read().strip().split("\n")
@@ -6,4 +16,10 @@ with open("input.txt") as f:
 cards = [list(map(int, re.findall(r"\d+", line))) for line in lines]
 wins = [len(set(card[11:]) & set(card[1:11])) for card in cards]
 
-print(sum(2 ** (match_count - 1) for match_count in wins if match_count > 0))
+card_counts = defaultdict(int)
+for i, win in enumerate(wins, 1):
+    card_counts[i] += 1
+    for j in range(i + 1, i + win + 1):
+        card_counts[j] += card_counts[i]
+
+print(sum(card_counts.values()))
