3db3df8d0d8f00db3dfe84e6d6ad46cb1435e592
Parth Jain <42parth.jain@gmail.com>
2024-06-22 22:35:58 +0530

    part1-day12

---
+++ 2023/day12/main.py
@@ -0,0 +1,32 @@
+import itertools
+
+total_arrangements = 0
+with open('input.txt') as f:
+    for row in f:
+        springs, groups = row.strip().split()
+        groups = list(map(int, groups.split(',')))
+        unknowns = springs.count('?')
+        arrangements = 0
+        for p in itertools.product('.#', repeat=unknowns):
+            new_springs = list(springs)
+            i = 0
+            for j, c in enumerate(new_springs):
+                if c == '?':
+                    new_springs[j] = p[i]
+                    i += 1
+            broken_groups = []
+            current_group = 0
+            for c in new_springs:
+                if c == '#':
+                    current_group += 1
+                else:
+                    if current_group > 0:
+                        broken_groups.append(current_group)
+                        current_group = 0
+            if current_group > 0:
+                broken_groups.append(current_group)
+            if broken_groups == groups:
+                arrangements += 1
+        total_arrangements += arrangements
+
+print(total_arrangements)
