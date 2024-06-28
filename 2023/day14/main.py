6b1ff95bc79dfbb32118020c4eca605811aeaa91
Parth Jain <42parth.jain@gmail.com>
2024-06-28 21:54:52 +0530

    part1-day14

--- 2023/day14/main.py
+++ 2023/day14/main.py
@@ -1,15 +1,17 @@
-with open("input.txt") as f:
-    ls = f.read().strip().split("\n")
+with open('input.txt') as f:
+    q = f.read().strip()
 
-board = {i + 1j * j: x for i, l in enumerate(ls) for j, x in enumerate(l)}
-blocked = {loc for loc, val in board.items() if val == "#"}
-rounds = {loc for loc, val in board.items() if val == "O"}
-
-while True:
-    free = board.keys() - rounds - blocked
-    newrounds = {z - 1 if z - 1 in free else z for z in rounds}
-    if newrounds == rounds:
-        break
-    rounds = newrounds
-
-print(sum(100 - z.real for z in rounds))
+platform = [list(x) for x in q.split('\n')]
+platform.insert(0, ['#']*len(platform[0]))
+for i in range(len(platform)):
+    for j in range(len(platform[0])):
+        if platform[i][j] == 'O':
+            for k in range(i-1, -1, -1):
+                if platform[k][j] == '#':
+                    break
+                elif platform[k][j] == '.':
+                    platform[k][j] = 'O'
+                    platform[k+1][j] = '.'
+            else:
+                platform[0][j] = '.'
+print('Part 1:',sum([sum([1 for x in p if x == 'O'])*(i+1) for i,p in enumerate(platform[::-1])]))
