7378184c942a44050d3edbae41dcba0c3ec67b65
Parth Jain <42parth.jain@gmail.com>
2024-07-01 20:05:08 +0530

    part1-day16

--- 2023/day16/main.py
+++ 2023/day16/main.py
@@ -1,29 +1,35 @@
-with open('input.txt', 'r') as f:
-    grid = [list(line.strip()) for line in f.readlines()]
-beams = [(0, 0, 0, 1)]  # (x, y, dx, dy)
+from collections import deque
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as file:
+    grid = [line.strip() for line in file]
+
+rows, cols = len(grid), len(grid[0])
 energized = set()
+beams = deque([(0, 0, 0, 1)])
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
 while beams:
-    new_beams = []
-    for x, y, dx, dy in beams:
-        if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
-            if (x, y) not in energized:
-                energized.add((x, y))
-            if grid[y][x] == '.':
-                new_beams.append((x + dx, y + dy, dx, dy))
-            elif grid[y][x] in ['/', '\\']:
-                if grid[y][x] == '/':
-                    dx, dy = -dy, dx
-                else:
-                    dx, dy = dy, -dx
-                new_beams.append((x + dx, y + dy, dx, dy))
-            elif grid[y][x] in ['|', '-']:
-                if dx == 0:
-                    new_beams.append((x + 1, y, 1, 0))
-                    new_beams.append((x - 1, y, -1, 0))
-                else:
-                    new_beams.append((x, y + 1, 0, 1))
-                    new_beams.append((x, y - 1, 0, -1))
-        else:
-            continue
-    beams = new_beams
-print(len(energized))
+    r, c, dr, dc = beams.popleft()
+
+    while 0 <= r < rows and 0 <= c < cols:
+        energized.add((r, c))
+        char = grid[r][c]
+
+        if char == '/' or char == '\\':
+            dr, dc = -dc, -dr
+            if char == '\\':
+                dr, dc = -dr, -dc
+        elif char == '|' and dc != 0:
+            beams.append((r, c, -1, 0))
+            beams.append((r, c, 1, 0))
+            break
+        elif char == '-' and dr != 0:
+            beams.append((r, c, 0, -1))
+            beams.append((r, c, 0, 1))
+            break
+
+        r, c = r + dr, c + dc
+
+print(f"Number of energized tiles: {len(energized)}")
